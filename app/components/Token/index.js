6974dcb316f10fa1e0e320991ac96ad1c833ea69
Pavel Svitek <svitek.pavel@gmail.com>
2018-10-28 11:05:25 +0100

    implement Token.transfering prop

--- app/components/Token/index.js
+++ app/components/Token/index.js
@@ -10,32 +10,45 @@ import { Card, Icon } from 'antd';
 import Layout from 'components/Layout';
 
 function Token(props) {
-  const { locked } = props;
+  const { transfering, locked } = props;
   const iconType = locked ? 'lock' : 'unlock';
   const toneColor = locked ? '#eb2f96' : '#52c41a';
 
   return (
-    <Card title={`Token: ${props.name}`}>
-      <Layout padding={false}>
-        <Icon
-          type={iconType}
-          theme="twoTone"
-          twoToneColor={toneColor}
-          style={{ fontSize: props.size }}
-        />
-      </Layout>
-    </Card>
+    <a onClick={props.onClick}>
+      <Card title={`Token: ${props.name}`}>
+        <Layout padding={false}>
+          {!transfering && (
+            <Icon
+              type={iconType}
+              theme="twoTone"
+              twoToneColor={toneColor}
+              style={{ fontSize: props.size }}
+            />
+          )}
+          {transfering && (
+            <Icon type="loading" style={{ fontSize: props.size }} />
+          )}
+        </Layout>
+      </Card>
+    </a>
   );
 }
 
 Token.propTypes = {
   locked: PropTypes.bool,
+  transfering: PropTypes.bool,
+  name: PropTypes.string,
   size: PropTypes.number,
+  onClick: PropTypes.func,
 };
 
 Token.defaultProps = {
   locked: false,
+  transfering: false,
+  name: 'undefined',
   size: 50,
+  onClick: () => {},
 };
 
 export default Token;
