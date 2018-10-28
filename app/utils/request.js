3dc2eb860ad930c6ae7b9e32161593b933ebbcdd
Pavel Svitek <svitek.pavel@gmail.com>
2018-10-28 09:08:53 +0100

    implement utils/request.js

---
+++ app/utils/request.js
@@ -0,0 +1,44 @@
+function parseJSON(response) {
+  if (response.status === 204 || response.status === 205) {
+    return null;
+  }
+  return response.json();
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
+
+export function request(url, options) {
+  return fetch(url, options)
+    .then(checkStatus)
+    .then(parseJSON);
+}
+
+export function get(url) {
+  return request(url, {});
+}
+
+export function post(url, data = {}) {
+  return fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      // "Content-Type": "application/x-www-form-urlencoded",
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+    .then(checkStatus)
+    .then(parseJSON); // parses response to JSON
+}
