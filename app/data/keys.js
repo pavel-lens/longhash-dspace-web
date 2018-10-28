83a317411de862a6afae79b7e62d4bd0dc090335
Pavel Svitek <svitek.pavel@gmail.com>
2018-10-28 11:05:04 +0100

    added keys to app/data/keys.js

---
+++ app/data/keys.js
@@ -0,0 +1,16 @@
+const keys = {
+  master: {
+    private: '5Hux59g3BEtRjHHTX8GAyiX4VuuMwzbahTmXziCSLDjVujGnhjb',
+    public: 'EVT6yzfViLh6ADndrGxCuBApjFDxuKhR6YjRqAszwM7anVTBHzrT4',
+  },
+  host: {
+    private: '5JFi7roAjp94SXCbq6jZ2TpULTD3jxy79JRiNk9twGqjv9V6qzo',
+    public: 'EVT6keccjv6kP2rUpzTFLJhL1ePU3uZBixuTfaYhGvKGqtvrHb9HA',
+  },
+  rentee: {
+    private: '5JdDhkeXMPAWkw1zL63EUAzSBQJ5hra2vzNKEd5axQb6rPTrBaB',
+    public: 'EVT5F4aHLTutvmYm6UGq4kB4rKV7ypi6ShY13MKxVVXyxKjU4RtQd',
+  },
+};
+
+export default keys;
